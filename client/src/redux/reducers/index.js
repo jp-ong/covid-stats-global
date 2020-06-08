66aaacc9c5ef9b8a8c4e26db0521d70fe4615ed5
John Paul Ong <jpong5202@gmail.com>
2020-06-08 23:08:39 +0800

    Changed reducer names

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,4 +1,4 @@
 import { combineReducers } from "redux";
 import statReducer from "./statReducer";
 
-export default combineReducers({ statReducer });
+export default combineReducers({ stats: statReducer });
